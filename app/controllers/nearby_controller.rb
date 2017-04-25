1042f9ef5f18a5e90c839df0ef42a80dbd72ef80
David Slone <dbslone@gmail.com>
2017-04-25 12:17:32 -0700

    update tests

--- app/controllers/nearby_controller.rb
+++ app/controllers/nearby_controller.rb
@@ -5,10 +5,6 @@ class NearbyController < ApplicationController
     start_date = Date.parse(params[:startDate])
     end_date = Date.parse(params[:endDate])
 
-    start_date2 = Date.parse('2017-03-24')
-    end_date2 = Date.parse('2017-04-24')
-
-Rails.logger.info("start: #{start_date} :: #{start_date2}")
     quakes_felt = []
 
     CSV.foreach('public/all_month.csv', headers: true) do |row|
@@ -24,8 +20,6 @@ Rails.logger.info("start: #{start_date} :: #{start_date2}")
         if magnitude_distance >= total_distance
           quakes_felt << {
             location: row[13],
-            lat: lat,
-            long: long,
             distance: total_distance,
             date: quake_date,
             magnitude: row[4]
--- test/controllers/nearby_controller_test.rb
+++ test/controllers/nearby_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class NearbyControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get nearby_index_url
+    get nearby_index_url, params: { startDate: '2017-03-24', endDate: '2017-04-24' }
     assert_response :success
   end
 
+  test "should parse csv and return results" do
+    get nearby_index_url, params: { startDate: '2017-03-24', endDate: '2017-04-24' }
+    assert_equal response.parsed_body.length, 8
+  end
 end
